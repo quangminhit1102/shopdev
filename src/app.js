0aee5c11736e26de9b8a20f380ce5776b7176291
Nguyễn Quang Minh <86247603+quangminhit1102@users.noreply.github.com>
2025-01-27 21:32:35 +0700

    Update comprsession note.

--- src/app.js
+++ src/app.js
@@ -22,7 +22,9 @@ app.use(morgan("combined"));
 app.use(helmet());
 
 // Compression
-app.use(compression);
+/* Reduced Data Transfer Size: Compression significantly reduces the size of data sent over the network, typically achieving 60-80% reduction for text-based content.
+app.use(compression); */
+
 
 // Init database
 
