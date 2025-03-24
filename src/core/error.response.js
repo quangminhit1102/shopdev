120b9eac58fcc06dd3aa5dec31d1f372d86e297a
Minh Nguyễn Quang <quangminhit1102@gmail.com>
2025-03-24 23:34:17 +0700

    Add throw exception for access service

--- src/core/error.response.js
+++ src/core/error.response.js
@@ -1,64 +1,89 @@
-'use strict';
+"use strict";
 
 const StatusCode = {
-    FORBIDDEN: 403,
-    CONFLICT: 409,
-    UNAUTHORIZED: 401,
-    NOT_FOUND: 404,
-    BAD_REQUEST: 400,
-    INTERNAL_SERVER: 500
-}
+  FORBIDDEN: 403,
+  CONFLICT: 409,
+  UNAUTHORIZED: 401,
+  NOT_FOUND: 404,
+  BAD_REQUEST: 400,
+  INTERNAL_SERVER: 500,
+};
 
 const ReasonStatusCode = {
-    FORBIDDEN: 'Bad request error',
-    CONFLICT: 'Conflict error',
-    UNAUTHORIZED: 'Unauthorized',
-    NOT_FOUND: 'Not found',
-    BAD_REQUEST: 'Bad request',
-    INTERNAL_SERVER: 'Internal Server Error'
-}
+  FORBIDDEN: "Bad request error",
+  CONFLICT: "Conflict error",
+  UNAUTHORIZED: "Unauthorized",
+  NOT_FOUND: "Not found",
+  BAD_REQUEST: "Bad request",
+  INTERNAL_SERVER: "Internal Server Error",
+};
 
 class ErrorResponse extends Error {
-    constructor(message, status) {
-        super(message)
-        this.status = status
-    }
+  constructor(message, status) {
+    super(message);
+    this.status = status;
+  }
 }
 
 class ConflictRequestError extends ErrorResponse {
-    constructor(message = ReasonStatusCode.CONFLICT, statusCode = StatusCode.CONFLICT) {
-        super(message, statusCode)
-    }
+  constructor(
+    message = ReasonStatusCode.CONFLICT,
+    statusCode = StatusCode.CONFLICT
+  ) {
+    super(message, statusCode);
+  }
 }
 
 class BadRequestError extends ErrorResponse {
-    constructor(message = ReasonStatusCode.BAD_REQUEST, statusCode = StatusCode.BAD_REQUEST) {
-        super(message, statusCode)
-    }
+  constructor(
+    message = ReasonStatusCode.BAD_REQUEST,
+    statusCode = StatusCode.BAD_REQUEST
+  ) {
+    super(message, statusCode);
+  }
 }
 
 class AuthFailureError extends ErrorResponse {
-    constructor(message = ReasonStatusCode.UNAUTHORIZED, statusCode = StatusCode.UNAUTHORIZED) {
-        super(message, statusCode)
-    }
+  constructor(
+    message = ReasonStatusCode.UNAUTHORIZED,
+    statusCode = StatusCode.UNAUTHORIZED
+  ) {
+    super(message, statusCode);
+  }
 }
 
 class NotFoundError extends ErrorResponse {
-    constructor(message = ReasonStatusCode.NOT_FOUND, statusCode = StatusCode.NOT_FOUND) {
-        super(message, statusCode)
-    }
+  constructor(
+    message = ReasonStatusCode.NOT_FOUND,
+    statusCode = StatusCode.NOT_FOUND
+  ) {
+    super(message, statusCode);
+  }
 }
 
 class ForbiddenError extends ErrorResponse {
-    constructor(message = ReasonStatusCode.FORBIDDEN, statusCode = StatusCode.FORBIDDEN) {
-        super(message, statusCode)
-    }
+  constructor(
+    message = ReasonStatusCode.FORBIDDEN,
+    statusCode = StatusCode.FORBIDDEN
+  ) {
+    super(message, statusCode);
+  }
+}
+
+class InternalServerError extends ErrorResponse {
+  constructor(
+    message = ReasonStatusCode.INTERNAL_SERVER,
+    statusCode = StatusCode.INTERNAL_SERVER
+  ) {
+    super(message, statusCode);
+  }
 }
 
 module.exports = {
-    ConflictRequestError,
-    BadRequestError,
-    AuthFailureError,
-    NotFoundError,
-    ForbiddenError
-}
+  ConflictRequestError,
+  BadRequestError,
+  AuthFailureError,
+  NotFoundError,
+  ForbiddenError,
+  InternalServerError,
+};
--- src/services/access.service.js
+++ src/services/access.service.js
@@ -8,7 +8,10 @@ const { log } = require("console");
 const KeyTokenService = require("./keyToken.service");
 const keyTokenModel = require("../models/keyToken.model");
 const { getObjectInformation } = require("../utils");
-const { BadRequestError } = require("../core/error.response");
+const {
+  BadRequestError,
+  InternalServerError,
+} = require("../core/error.response");
 
 class AccessService {
   static async getAccess() {
@@ -22,19 +25,13 @@ class AccessService {
       // Replace this with actual user validation
       const userInfo = await shopModel.findOne({ email: email }).lean().exec();
       if (!userInfo) {
-        return {
-          code: 400,
-          message: "Invalid email or password",
-        };
+        throw new BadRequestError("Invalid email or password");
       }
 
       // Compare password
       const match = await bcrypt.compare(password, userInfo.password);
       if (!match) {
-        return {
-          code: 400,
-          message: "Invalid email or password",
-        };
+        throw new BadRequestError("Invalid email or password");
       }
 
       // Get private key from database
@@ -56,120 +53,100 @@ class AccessService {
         refreshToken,
       };
     } catch (error) {
-      return {
-        code: 400,
-        message: "Login failed",
-        error: error.message,
-      };
+      throw new InternalServerError("Failed to login");
     }
   }
 
   // Register a new User
   static async register({ name, email, password }) {
-    try {
-      // Hash password
-      const saltRounds = 10;
-      const hashedPassword = await bcrypt.hash(password, saltRounds);
-      password = hashedPassword;
+    // Hash password
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    password = hashedPassword;
 
-      // Email already exists
-      const existingUser = await shopModel
-        .findOne({ email })
-        .lean() // .lean() to return a plain JavaScript object instead of a Mongoose document
-        .exec(); // .exec() to execute the query and return a promise;
-      if (existingUser) {
-        throw new BadRequestError("Email already exists");
-      }
+    // Email already exists
+    const existingUser = await shopModel
+      .findOne({ email })
+      .lean() // .lean() to return a plain JavaScript object instead of a Mongoose document
+      .exec(); // .exec() to execute the query and return a promise;
+    if (existingUser) {
+      throw new BadRequestError("Email already exists");
+    }
 
-      const newShop = await shopModel.create({
-        name,
-        email,
-        password, // Should be hashed before storing
-        verified: false,
+    const newShop = await shopModel.create({
+      name,
+      email,
+      password, // Should be hashed before storing
+      verified: false,
+    });
+    if (newShop) {
+      log(`New shop created: ${newShop}`);
+      // Create private key and public key for shop
+      /**
+       * PEM format example for public key:
+       * -----BEGIN PUBLIC KEY-----
+       * MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA1234567890...
+       * -----END PUBLIC KEY-----
+       *
+       * PEM format example for private key:
+       * -----BEGIN PRIVATE KEY-----
+       * MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC1234567890...
+       * -----END PRIVATE KEY-----
+       */
+      const { privateKey, publicKey } = crypto.generateKeyPairSync("rsa", {
+        modulusLength: 2048, // Key size in bits
+        publicKeyEncoding: {
+          type: "spki", // Recommended format for public keys - Subject Public Key Info
+          format: "pem", // PEM format (most common)
+        },
+        privateKeyEncoding: {
+          type: "pkcs8", // Recommended format for private keys - Public-Key Cryptography Standards
+          format: "pem", // PEM format (most common)
+        },
       });
 
-      if (newShop) {
-        log(`New shop created: ${newShop}`);
-        // Create private key and public key for shop
-        /**
-         * PEM format example for public key:
-         * -----BEGIN PUBLIC KEY-----
-         * MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA1234567890...
-         * -----END PUBLIC KEY-----
-         *
-         * PEM format example for private key:
-         * -----BEGIN PRIVATE KEY-----
-         * MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC1234567890...
-         * -----END PRIVATE KEY-----
-         */
-        const { privateKey, publicKey } = crypto.generateKeyPairSync("rsa", {
-          modulusLength: 2048, // Key size in bits
-          publicKeyEncoding: {
-            type: "spki", // Recommended format for public keys - Subject Public Key Info
-            format: "pem", // PEM format (most common)
-          },
-          privateKeyEncoding: {
-            type: "pkcs8", // Recommended format for private keys - Public-Key Cryptography Standards
-            format: "pem", // PEM format (most common)
-          },
-        });
+      log(`publicKey: ${publicKey}`);
+      log(`privateKey: ${privateKey}`);
 
-        log(`publicKey: ${publicKey}`);
-        log(`privateKey: ${privateKey}`);
-
-        // Save public key to database
-        const publicKeyString = await KeyTokenService.createKeyToken({
-          userId: newShop._id,
-          publicKey,
-          privateKey,
-        });
-        if (!publicKeyString) {
-          return {
-            code: 500,
-            message: "Failed to create public key",
-          };
-        }
-
-        const { token, refreshToken } = await authUtils.createTokenPair(
-          getObjectInformation(["_id", "email", "name"], newShop),
-          publicKey,
-          privateKey
-        );
-
-        log(`token: ${token}`);
-        log(`refreshToken: ${refreshToken}`);
-
-        // Send verification email
-        // Implement email sending logic here
-
-        return {
-          code: 201,
-          message:
-            "Registration successful. Please check your email to verify account.",
-          data: {
-            shop: {
-              _id: newShop._id,
-              email: newShop.email,
-              name: newShop.name,
-              email: newShop.email,
-            },
-            token,
-            refreshToken,
-          },
-        };
-      } else {
-        return {
-          code: 500,
-          message: "Registration failed",
-        };
+      // Save public key to database
+      const publicKeyString = await KeyTokenService.createKeyToken({
+        userId: newShop._id,
+        publicKey,
+        privateKey,
+      });
+      if (!publicKeyString) {
+        throw new InternalServerError("Failed to save public key");
       }
-    } catch (error) {
+
+      const { token, refreshToken } = await authUtils.createTokenPair(
+        getObjectInformation(["_id", "email", "name"], newShop),
+        publicKey,
+        privateKey
+      );
+
+      log(`token: ${token}`);
+      log(`refreshToken: ${refreshToken}`);
+
+      // Send verification email
+      // Implement email sending logic here
+
       return {
-        success: false,
-        message: "Registration failed",
-        statusCode: 500,
-        errors: error.message,
+        code: 201,
+        message:
+          "Registration successful. Please check your email to verify account.",
+        data: {
+          shop: {
+            _id: newShop._id,
+            email: newShop.email,
+            name: newShop.name,
+            email: newShop.email,
+          },
+          token,
+          refreshToken,
+        },
       };
+    } else {
+      throw new InternalServerError("Failed to registration");
     }
   }
 }
