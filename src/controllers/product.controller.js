37ecb5b80418ad54d887a5be0e91cd283f967cde
Minh Nguyễn Quang <quangminhit1102@gmail.com>
2025-04-15 22:49:47 +0700

    Add product controller for new features

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -10,8 +10,43 @@ const { CREATED } = require("../core/success.response");
 class ProductController {
   createProduct = async (req, res) => {
     new CREATED({
-      message: "Product created successfully",
+      message: "Product created successfully!",
       metadata: await ProductStrategy.createProduct(req.body),
+    }).send(res);
+  };
+
+  publishProduct = async (req, res) => {
+    new CREATED({
+      message: "Product published successfully!",
+      metadata: await ProductStrategy.publishProduct(req.body),
+    }).send(res);
+  };
+
+  unPublishProduct = async (req, res) => {
+    new CREATED({
+      message: "Product unpublished successfully!",
+      metadata: await ProductStrategy.unPublishProduct(req.body),
+    }).send(res);
+  };
+
+  findAllDraftProductsOfShop = async (req, res) => {
+    new CREATED({
+      message: "Draft products retrieved successfully!",
+      metadata: await ProductStrategy.findAllDraftProductsOfShop(req.body),
+    }).send(res);
+  };
+
+  findAllPublishedProductsOfShop = async (req, res) => {
+    new CREATED({
+      message: "Published products retrieved successfully!",
+      metadata: await ProductStrategy.findAllPublishedProductsOfShop(req.body),
+    }).send(res);
+  };
+
+  searchProducts = async (req, res) => {
+    new CREATED({
+      message: "Products retrieved successfully!",
+      metadata: await ProductStrategy.searchProducts(req.body),
     }).send(res);
   };
 }
