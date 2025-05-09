5606b74097c77c0ba5ad4cd71d20941c151a76e0
Minh Nguyễn Quang <quangminhit1102@gmail.com>
2025-05-09 23:16:24 +0700

    Update Cart service

--- src/services/cart.service.js
+++ src/services/cart.service.js
@@ -37,16 +37,15 @@ class CartService {
   }
 
   static async updateUserCart({ user_id, product }) {
+    const { productId, quantity } = product;
     const query = {
       cart_userId: user_id,
       cart_state: "active",
+      "cart_products.productId": productId,
     };
     const updateOrInsert = {
-      $addToSet: {
-        cart_products: product,
-      },
-      $set: {
-        cart_count_product: 1,
+      $inc: {
+        "cart_products.$[product].quantity": quantity,
       },
     };
     const options = {
@@ -61,7 +60,31 @@ class CartService {
 
   static async addToCart({ user_id, product = {} }) {
     // Check if the user has a cart -> If not, create a new cart
+    const cart = await cartModel.findOne({
+      cart_userId: user_id,
+      cart_state: "active",
+    });
+    if (!cart) {
+      return await this.createUserCart({ user_id, product });
+    }
     // Check if the product is already in the cart -> If yes, increase the quantity
     // If not, add the product to the cart
+    const productInCart = cart.cart_products.find(
+      (item) => item.productId === product.productId
+    );
+    if (productInCart) {
+      return await this.updateUserCart({ user_id, product });
+    } else {
+      // If the product is not in the cart, add it to the cart
+      const updatedCart = await cartModel.findOneAndUpdate(
+        { cart_userId: user_id, cart_state: "active" },
+        {
+          $addToSet: { cart_products: product },
+          $inc: { cart_count_product: 1 },
+        },
+        { new: true }
+      );
+      return updatedCart;
+    }
   }
 }
