e722a9c0037fd7ac68a53cc42ec8017f101be949
Minh Nguyễn Quang <quangminhit1102@gmail.com>
2025-05-10 15:20:43 +0700

    Add Cart service

--- src/services/cart.service.js
+++ src/services/cart.service.js
@@ -1,6 +1,8 @@
 "use strict";
 
 const cartModel = require("../models/cart.model");
+const productRepository = require("../models/repositories/product.repo");
+const CartService = require("./cart.service");
 
 /* 
     Key features: Cart service
@@ -65,26 +67,106 @@ class CartService {
       cart_state: "active",
     });
     if (!cart) {
-      return await this.createUserCart({ user_id, product });
+      return await CartService.createUserCart({ user_id, product });
     }
     // Check if the product is already in the cart -> If yes, increase the quantity
     // If not, add the product to the cart
-    const productInCart = cart.cart_products.find(
-      (item) => item.productId === product.productId
-    );
-    if (productInCart) {
-      return await this.updateUserCart({ user_id, product });
-    } else {
-      // If the product is not in the cart, add it to the cart
-      const updatedCart = await cartModel.findOneAndUpdate(
-        { cart_userId: user_id, cart_state: "active" },
-        {
-          $addToSet: { cart_products: product },
-          $inc: { cart_count_product: 1 },
-        },
-        { new: true }
-      );
-      return updatedCart;
+    // If the product is already in the cart, increase the quantity
+    if (!cart.cart_products.length) {
+      cart.cart_products.push(product);
+      return await cart.save();
     }
+
+    return await CartService.updateUserCart({ user_id, product });
+  }
+
+  /*
+  shop_order_ids: [
+  {
+    item_products: [
+      {
+        shop_id,
+        product_id,
+        quantity,
+        old_quantity,
+        price,
+        product_id,
+        product_name,
+      },
+    ],
+  }]
+  */
+  static async addToCartV2({ user_id, product = {} }) {
+    const { proudct_id, quantity, old_quantity } =
+      shop_order_ids[0].item_products[0];
+
+    // Check product in DB
+    const foundProduct = await productRepository.findProductById({
+      product_id: proudct_id,
+    });
+    if (!foundProduct) throw new Error("Product not found");
+
+    if (quantity === 0) {
+      // Remove product from cart
+      const query = {
+        cart_userId: user_id,
+        cart_state: "active",
+        "cart_products.productId": proudct_id,
+      };
+      const updateOrInsert = {
+        $pull: {
+          cart_products: { productId: proudct_id },
+        },
+      };
+      const options = {
+        upsert: true,
+        new: true,
+      };
+
+      return await cartModels
+        .findOneAndUpdate(query, updateOrInsert, options)
+        .exec();
+    }
+
+    return await CartService.updateUserCart({
+      user_id,
+      product: {
+        product_id: proudct_id,
+        quantity: quantity - old_quantity,
+        name: foundProduct.product_name,
+        price: foundProduct.product_price,
+      },
+    });
+  }
+
+  static async deleteUserCart({ user_id, product_id }) {
+    const query = {
+      cart_userId: user_id,
+      cart_state: "active",
+      "cart_products.productId": product_id,
+    };
+    const updateOrInsert = {
+      $pull: {
+        cart_products: { productId: product_id },
+      },
+    };
+    const options = {
+      upsert: true,
+      new: true,
+    };
+
+    return await cartModel
+      .findOneAndUpdate(query, updateOrInsert, options)
+      .exec();
+  }
+
+  static async getUserCart({ user_id }) {
+    const query = {
+      cart_userId: user_id,
+      cart_state: "active",
+    };
+    return await cartModel.findOne(query).exec();
   }
 }
+
+modele.exports = CartService;
