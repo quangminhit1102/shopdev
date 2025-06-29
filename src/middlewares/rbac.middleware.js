d667c4e865907b33883b6473f33641bcd03bad71
Minh Nguyễn Quang <quangminhit1102@gmail.com>
2025-06-29 23:13:20 +0700

    Add RBAC middleware

---
+++ src/middlewares/rbac.middleware.js
@@ -0,0 +1,25 @@
+const { ForbiddenError } = require("../core/error.response");
+const rbac = require("./role.middleware"); // Import the AccessControl instance
+
+const grantAccess = (action, resource) => {
+  return async (req, res, next) => {
+    try {
+      const role_name = req.query.role || "guest"; // Default to 'guest' if no role is provided
+      const permission = rbac.can(role_name)[action](resource);
+
+      if (!permission.granted) {
+        throw new ForbiddenError(
+          `You don't have enough permission to perform this action`
+        );
+      }
+
+      next();
+    } catch (error) {
+      next(error);
+    }
+  };
+};
+
+module.exports = {
+  grantAccess,
+};
