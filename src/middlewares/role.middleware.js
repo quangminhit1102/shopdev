1e9269db6debf07a1b731d5c47b894b7a73e3bff
Minh Nguyễn Quang <quangminhit1102@gmail.com>
2025-06-29 23:05:49 +0700

    Add RBAC with access control npm

---
+++ src/middlewares/role.middleware.js
@@ -0,0 +1,12 @@
+"use strict";
+const AccessControl = require("accesscontrol");
+let grantList = [
+  {
+    role: "admin",
+    resource: "profile",
+    action: "read:any",
+    attributes: "*, !views",
+  },
+  { role: "shop", resource: "video", action: "read:own", attributes: "*" },
+];
+module.exports = new AccessControl(grantList);
