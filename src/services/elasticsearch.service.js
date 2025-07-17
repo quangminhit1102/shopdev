f362f531470bae2d358d7b463cc5bc8843e3ca18
Minh Nguyễn Quang <quangminhit1102@gmail.com>
2025-07-17 23:47:15 +0700

    Create index for elasticsearch

---
+++ src/services/elasticsearch.service.js
@@ -0,0 +1,7 @@
+const elasticClient = require("./../dbs/elastic-client");
+
+const createIndex = async (indexName) => {
+  await elasticClient.indices.create({ index: indexName });
+  console.log("Index created");
+};
+createIndex("posts");
