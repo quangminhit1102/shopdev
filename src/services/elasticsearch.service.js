c2ad70bc1cf6e17769338ac6a6295ad449811cad
Minh Nguyễn Quang <quangminhit1102@gmail.com>
2025-07-19 23:51:13 +0700

    Add elasticsearch create index file

--- src/services/elasticsearch.service.js
+++ src/services/elasticsearch.service.js
@@ -4,4 +4,11 @@ const createIndex = async (indexName) => {
   await elasticClient.indices.create({ index: indexName });
   console.log("Index created");
 };
+
+const deleteIndex = async (indexName) => {
+  await elasticClient.indices.delete({ index: indexName });
+  console.log("Index deleted");
+};
+
+deleteIndex("posts");
 createIndex("posts");
