be9b886fc7cca15bc26b32d8a48b5559ce6d9531
Minh Nguyễn Quang <quangminhit1102@gmail.com>
2025-07-30 23:10:07 +0700

    Add IORedis prevent Cache penetration example for SKU

--- src/services/sku.service.js
+++ src/services/sku.service.js
@@ -1,9 +1,17 @@
 "use strict";
 const skuModel = require("../models/sku.model");
 const { NotFoundError } = require("../core/error.response");
-const { getRedisClient, initRedis } = require("../dbs/init.redis");
+// const { getRedisClient, initRedis } = require("../dbs/init.redis");
+const { getRedisClient } = require("../dbs/init.ioredis");
+const {
+  setCacheIOExpiration,
+  getCacheIO,
+} = require("../models/repositories/cache.redis.repo");
+
+const CACHE_PRODUCT_SKU = "product:sku";
 
 const GetSKU = async ({ sku_id, product_id }) => {
+  // ================= Cache redis ======================
   // // Ensure Redis is initialized
   // let redisClient = getRedisClient();
   // if (!redisClient) {
@@ -17,19 +25,45 @@ const GetSKU = async ({ sku_id, product_id }) => {
   //   return JSON.parse(cached);
   // }
 
-  // Find sku in DB (use correct field names)
-  const sku = await skuModel.findOne({
-    sku_id,
-    product_id,
-  });
-  if (!sku) {
-    throw new NotFoundError("SKU not found");
+  // ================ Cache penetration ================
+  // 1. Check input parameters
+  if (!sku_id || !product_id) {
+    return null; // or throw an error if you prefer
+  }
+
+  // 2. Check if SKU exists in cache
+  const skuKeyCache = `CACHE_PRODUCT_SKU_${sku_id}`;
+  const skuCache = await getCacheIO({ key: skuKeyCache });
+  if (skuCache) {
+    console.log(`GET SKU from cache REDIS::${skuCache}`);
+    return {
+      ...JSON.parse(skuCache),
+      loadFrom: "cache",
+    };
+  }
+  // 3. If not in cache, proceed to DB query
+  else {
+    // Find sku in DB (use correct field names)
+    const sku = await skuModel
+      .findOne({
+        sku_id,
+        product_id,
+      })
+      .lean();
+
+    const valueCache = sku ? JSON.stringify(sku) : null;
+
+    setCacheIOExpiration(skuKeyCache, valueCache, 30); // Cache for 30 seconds
+    return {
+      ...JSON.parse(valueCache),
+      loadFrom: "database",
+    };
   }
 
   // // Cache the result for 5 minutes (300 seconds)
   // await redisClient.setEx(cacheKey, 300, JSON.stringify(sku));
 
-  return sku;
+  // return sku;
 };
 
 const GetSKUsByProductId = async ({ product_id }) => {
